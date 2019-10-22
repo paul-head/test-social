67255e98a10c8bb74b82888f6b672e5304bcc236
Pavel Golovanov <publichead@yandex.ru>
2019-10-23 00:22:33 +0300

    add image list/ image likes / ajax request decorator / ajax pagination

---
+++ common/decorators.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponseBadRequest
+
+
+def ajax_required(fn):
+    def wrap(request, *args, **kwargs):
+        if not request.is_ajax():
+            return HttpResponseBadRequest
+        return fn(request, *args, **kwargs)
+    wrap.__doc__=fn.__doc__
+    wrap.__name__=fn.__name__
+    return wrap
