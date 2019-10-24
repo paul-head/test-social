1699cd6aaeedda1f0b8e70ad6789f9699ee17691
Pavel Golovanov <publichead@yandex.ru>
2019-10-24 22:26:49 +0300

    add activity in news feed, code in another apps

--- account/views.py
+++ account/views.py
@@ -8,6 +8,7 @@ from django.contrib import messages
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile, Contact
 from common.decorators import ajax_required
+from action.utils import create_action
 
 
 # Create your views here.
@@ -46,6 +47,7 @@ def register(request):
             new_user = user_form.save(commit=False)
             new_user.set_password(user_form.cleaned_data['password'])
             Profile.objects.create(user=new_user)
+            create_action(request.user, 'Has created an account')
             new_user.save()
             return render(request, 'account/register_done.html', {'new_user': new_user})
     else:
@@ -101,6 +103,7 @@ def user_follow(request):
             user = User.objects.get(id=user_id)
             if action == 'follow':
                 Contact.objects.get_or_create(user_from=request.user, user_to=user)
+                create_action(request.user, 'is following', user)
             else:
                 Contact.objects.filter(user_from=request.user, user_to=user).delete()
             return JsonResponse({'status': 'ok'})
